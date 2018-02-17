77300f261d5c577143b07d1b750f588a15aa91b0
Matt Gohr <gohr89@gmail.com>
2018-02-17 17:33:19 -0500

    keys loaded

--- liri.js
+++ liri.js
@@ -1,4 +1,7 @@
 require("dotenv").config();
+var Twitter = require('twitter');
+var keys = require('./keys.js')
+
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
